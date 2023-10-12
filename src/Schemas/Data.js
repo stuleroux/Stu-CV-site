38425bcb60ac05632b86a9a02095f68892313ca4
Forrest Alward <forrestalward@gmail.com>
2023-10-12 08:31:47 +0200

    inital commit

---
+++ src/Schemas/Data.js
@@ -0,0 +1,139 @@
+export const Data = {
+  profile: {
+    name: "Stu Le Roux",
+    ocupation: "Full Time Sex Symbol",
+    location: "Cape Town, South Africa",
+    email: "lagos.matias25@gmail.com",
+    telephone: "+56 971999705",
+    image: "images/90208122_10222269974249078_3631065450546200576_n.jpg",
+  },
+  aboutMe: {
+    label: "Perfil",
+    description:
+      "Analista Programador, capacitado para desarrollar aplicaciones informáticas en diversas tecnologías y múltiples lenguajes de programación, aplicando metodologías ágiles de desarrollo, incluyendo identificación de necesidades y diseños de sistemas basados en modelos de desarrollo.",
+  },
+  skills: {
+    technicalLabel: "Tecnologías",
+    softLabel: "Skills",
+    technicalSkills: [
+      "Angular",
+      "React",
+      "Java",
+      "Nodejs",
+      "Spring Boot",
+      "Nestjs",
+      "APIM",
+      "SQL",
+      "Git",
+    ],
+    softSkills: [
+      "Capacidad de análisis",
+      "Resolución de Problemas",
+      "Pensamiento Crítico",
+      "Trabajo en equipo",
+      "Comunicación",
+      "Proactividad",
+      "Creatividad",
+      "Autodidacta",
+      "Mentoria",
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Maaato",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/matias-alfonso-lagos-gajardo",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "Maaato",
+        name: "github",
+        url: "https://github.com/Maaato",
+        className: "bxl-github",
+      },
+      {
+        label: "Maaato",
+        name: "twiiter",
+        url: "https://twitter.com/I_Maaato",
+        className: "bxl-twitter",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "FULL-STACK DEVELOPER",
+        period: "Oct. 2021 - Actualidad",
+        company: "Banco Ripley",
+        description: [
+          "Desarrollo y mejora continua de microservicios REST en Java y Node.js(JS/TS).",
+          "Desarrollo y mejora continua de PWA en las tecnologías Angular, Ionic y Cordova.",
+          "Canalización CI/CD con Bitbucket, Jenkins y AWS como infraestructura."
+        ],
+      },
+      {
+        title: "FULL-STACK DEVELOPER",
+        period: "Oct. 2019 - Oct. 2021",
+        company: "Altiuz",
+        description: [
+          "Desarrollo de Web Services REST en Java y Nodejs(JS/TS) acompañado de diferentes motores de BD como Mysql, Oracle, MariaDB, MongoDB e IBM API Connect y 3scale como API Gateway.",
+          "Desarrollo de Frontend y Mobile con tecnologías como Angular, React, React Native, Ionic y Cordova.",
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "Técnico de Nivel Superior Analista Programador",
+        date: "2019",
+        institution: "Universidad Tecnológica de Chile INACAP",
+      },
+    ],
+    proyects: [
+      {
+        name: "Onboarding Digital",
+        company: "Banco de Chile",
+        period: "Nov. 2019 - Ene. 2020",
+        description: [
+          "Control, desarrollo, securitización y despliegue de APIs en plataforma IBM API Connect.",
+        ],
+      },
+      {
+        name: "Captación Digital",
+        company: "Banco Ripley",
+        period: "Ene. 2020 - Feb. 2020",
+        description: [
+          "Control, desarrollo y securitización de APIs en plataforma IBM API Connect.",
+          "Desarrollo frontend en Angular para el consumo de Web Services SOAP/REST apuntando a ambientes DEV/PRE-PROD/PROD como simulación.",
+        ],
+      },
+      {
+        name: "Agendamiento Remoto",
+        company: "Gobierno de Chile",
+        period: "Mar. 2020 - May. 2020",
+        description: [
+          "Desarrollo frontend y mobile con las tecnologias de Angular 6 e Ionic 4 para el agendamiento de horas en centros a lo largo de todo Chile.",
+        ],
+      },
+      {
+        name: "Altiuz Report Dynamic",
+        company: "Altiuz",
+        period: "Jun. 2020 - Jul. 2020",
+        description: [
+          "Desarrollo de servicio Java para la generación unitaria de documentos, sin la necesidad de tener almacenada la información del mismo en una fuente de datos.",
+        ],
+      },
+      {
+        name: "Desarrollos Tigo",
+        company: "Computec S.A.S",
+        period: "Ago. 2020 - Dic 2020",
+        description: [
+          "Desarrollo Java/Spring Boot de Web Service API REST para consulta y descarga de documentos almacenados en repositorios IBM CMOD.",
+          "Desarrollo Java CRON para transferencia de documentos XML hacia servidor SFTP.",
+          "Desarrollo Java CRON para carga de documentos PDFs hacia Azure Blob Storage.",
+        ],
+      },
+    ],
+  },
+};
